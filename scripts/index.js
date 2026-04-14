d7ca813b7dc3d2a37c6e9100dcdf7287be5c5826
minwoo <jmw041615@gmail.com>
2026-04-14 18:16:27 +0900

    헤더, 혜택, 배너 완료

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const bannerSwiper = new Swiper('.banner_container .banner_wrap', {
+
+})
+const moreBtn = document.querySelector('.more_btn');
+moreBtn.addEventListener('click',()=>{
+    console.log('됐나?')
+})
