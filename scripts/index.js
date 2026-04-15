ffc50f1b149b4cbfca3ab606b5bad297380fc1f8
minwoo <jmw041615@gmail.com>
2026-04-15 17:12:18 +0900

    상품목록 진행 중

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,50 @@
+const cate = document.querySelectorAll('.all_product_container .all_cate_wrap li button');
+const allProduct = document.querySelectorAll('.all_product_list_wrap .swiper');
+console.log(cate, allProduct);
+cate.forEach((btn,i)=>{
+    btn.addEventListener('click',()=>{
+        cate.forEach((b)=>{
+            b.classList.remove('active');
+        })
+        btn.classList.add('active');
+        allProduct.forEach((s,index)=>{
+            if(i == index){
+                s.style.display = 'block';
+                const swiperInstance = s.swiper;
+                swiperInstance.slideTo(0);
+                swiperInstance.update();
+            }else {
+                s.style.display = 'none';
+            }
+        })
+    })
+})
+
+
+
+
+
 const bannerSwiper = new Swiper('.banner_container .banner_wrap', {
 
 })
 const tasteSwiper = new Swiper('.taste_list_wrap',{
     slidesPerView:3.4,
-    // centeredSlides: true
+    loop:true,
+    slidesPerGroup: 1,
+})
+const reviewSwiper = new Swiper('.review_list_wrap',{
+    slidesPerView:2,
+    spaceBetween:30,
+    navigation: {
+        nextEl: '.review_container .swiper-button-next',
+        prevEl: '.review_container .swiper-button-prev',
+    },
+})
+const allSwiper = new Swiper('.all_product_list_wrap .swiper',{
+    slidesPerView:4,
+    spaceBetween:73.33,
+    navigation: {
+        nextEl: '.all_product_list_wrap .swiper-button-next',
+        prevEl: '.all_product_list_wrap .swiper-button-prev',
+    },
 })
